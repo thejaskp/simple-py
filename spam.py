78c0cd5c521f7029727ba5607ce545b09281e16f
thejaskp <thejaskuttipoyil@gmail.com>
2023-01-22 18:04:06 +0530

    spam bot edited

--- spam.py
+++ spam.py
@@ -1,10 +1,12 @@
 import pyautogui as pag
 import time
 
+message = input("What is the message")
+msg_count = int(input("how many times you need to send the message"))
 time.sleep(4)
 count = 0
 
-while count < 200:
-    pag.typewrite("hi")
+while count < msg_count:
+    pag.typewrite(message)
     pag.press("enter")
     count = count + 1
